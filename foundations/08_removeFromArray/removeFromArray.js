4cb0bcbecd3ea6f08e6433b2aaefab56d27a137e
Behim13 <jakuposkii@gmail.com>
2026-01-21 19:52:29 +0100

    Finished 08 removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(el, ...args) {
+    let newArray = []
+    el.forEach((item) => {
+        if (!args.includes(item))
+            newArray.push(item)
+    })
+    return newArray
 };
 
 // Do not edit below this line
